ee5d4d0db4812fa1db07ea3fcc3a4e3c6ed53654
ghemantpravin <hemantgandhi12017@gmail.com>
2024-11-16 17:27:33 +0530

    theme is working fine. card bg is changing

---
+++ themeSwitcher/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react'
+import React from 'react'
+import './App.css'
+import Card from './components/Card'
+import ThemeBtn from './components/ThemeBtn'
+import { ThemeProvider } from './context/theme'
+// import { ThemeProvider } from './context/ThemeContextProvider'
+
+function App() {
+
+  const [themeMode, setthemeMode] = useState ('light')
+
+  const lightTheme = () => {
+    setthemeMode ('light')
+}   
+
+const darkTheme = () => {
+    setthemeMode ('dark')
+}
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove('light', 'dark')
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode]);
+
+
+  return (
+    
+        <ThemeProvider value = {{themeMode, darkTheme, lightTheme}}>
+          <div className="flex flex-wrap min-h-screen items-center">
+              <div className="w-full">
+                  <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                        <ThemeBtn></ThemeBtn>
+                  </div>
+
+                  <div className="w-full max-w-sm mx-auto">
+                      <Card></Card>
+                  </div>
+              </div>
+          </div>
+        </ThemeProvider>
+  )
+}
+
+export default App
